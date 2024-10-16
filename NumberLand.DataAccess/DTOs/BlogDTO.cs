e0a89dc8a86024a2fda85ca366d24ea45d2e6e0c
arsham_ae <arsham7.ae@gmail.com>
2024-10-16 20:30:20 +0330

    Merge DTOs

--- NumberLand.DataAccess/DTOs/BlogDTO.cs
+++ NumberLand.DataAccess/DTOs/BlogDTO.cs
@@ -20,4 +20,24 @@ namespace NumberLand.DataAccess.DTOs
         public DateTime publishedAt { get; set; }
         public bool isPublished { get; set; }
     }
+    public class CreateBlogDTO
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string content { get; set; }
+        public int authorId { get; set; }
+        public List<int> blogCategories { get; set; }
+        public string featuredImagePath { get; set; }
+        public DateTime createAt { get; set; } = DateTime.Now;
+        public DateTime updateAt { get; set; }
+        public DateTime publishedAt { get; set; }
+        public bool isPublished { get; set; }
+    }
+
+    public class BlogCategoryDTO
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+    }
 }
--- NumberLand.DataAccess/DTOs/NumberDTO.cs
+++ NumberLand.DataAccess/DTOs/NumberDTO.cs
@@ -19,4 +19,14 @@ namespace NumberLand.DataAccess.DTOs
         public DateTime expireTime { get; set; }
         public string price { get; set; }
     }
+    public class CreateNumberDTO
+    {
+        public int id { get; set; }
+        public string number { get; set; }
+        public string application { get; set; }
+        public int categoryId { get; set; }
+        public int operatorId { get; set; }
+        public DateTime expireTime { get; set; }
+        public string price { get; set; }
+    }
 }
