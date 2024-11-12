14f8c2efc2b05bae8d05ee21154d987296884064
arsham_ae <arsham7.ae@gmail.com>
2024-11-13 00:40:43 +0330

    [fix]: minor fix(Number Validation)

--- NumberLand/Validators/CreateNumberValidator.cs
+++ NumberLand/Validators/CreateNumberValidator.cs
@@ -1,23 +1,25 @@
 ﻿using FluentValidation;
 using NumberLand.Command.Number.Command;
+using NumberLand.Command.Number.Handler;
+using NumberLand.DataAccess.DTOs;
 
 namespace NumberLand.Validators
 {
-    public class CreateNumberValidator : AbstractValidator<CreateNumberCommand>
+    public class CreateNumberValidator : AbstractValidator<CreateNumberDTO>
     {
         public CreateNumberValidator()
         {
-            RuleFor(n => n.NumberDTO.number)
+            RuleFor(n => n.number)
                 .NotEmpty().WithMessage("Number Cannot Be Empty!")
                 .MaximumLength(15).WithMessage("Max Length Should be 15")
                 .Matches(@"^\+?[0-9]+$").WithMessage("Phone number must contain only numbers and the '+' sign.");
-            RuleFor(n => n.NumberDTO.expireTime)
+            RuleFor(n => n.expireTime)
                 .NotEmpty().WithMessage("Expire Time Cannot be Empty!")
                 .GreaterThan(DateTime.Now.AddMinutes(30)).WithMessage("Expire Time Cannot Be Les than 30Min!");
-            RuleFor(n => n.NumberDTO.operatorId)
+            RuleFor(n => n.operatorId)
                 .NotEmpty().WithMessage("OperatorId Cannot Be Empty!")
                 .GreaterThanOrEqualTo(1).WithMessage("OperatorId Should Be 1 Or Above!");
-            RuleFor(n => n.NumberDTO.categoryId)
+            RuleFor(n => n.categoryId)
                 .NotEmpty().WithMessage("CategoryId Cannot Be Empty!")
                 .GreaterThanOrEqualTo(1).WithMessage("CategoryId Should Be 1 Or Above!");
         }
