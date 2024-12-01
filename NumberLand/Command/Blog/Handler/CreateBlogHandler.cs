cf9d8c904a3254d945118d88fb2fe7657e8f453e
Arsham Ebrahimi <arsham7.ae@gmail.com>
2024-12-01 20:22:18 +0330

    [Fix]: Fix Posting Blog Issue(Fix Mapping)

--- NumberLand/Command/Blog/Handler/CreateBlogHandler.cs
+++ NumberLand/Command/Blog/Handler/CreateBlogHandler.cs
@@ -23,59 +23,70 @@ namespace NumberLand.Command.Blog.Handler
         }
         public async Task<CommandsResponse<BlogDTO>> Handle(CreateBlogCommand request, CancellationToken cancellationToken)
         {
-            var uploadsFolder = Path.Combine(_environment.WebRootPath, "images", "blogs");
-            if (!Directory.Exists(uploadsFolder))
+            try
             {
-                Directory.CreateDirectory(uploadsFolder);
-            }
-            var uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(request.File.FileName);
-            var filePath = Path.Combine(uploadsFolder, uniqueFileName);
-
-            using (var fileStream = new FileStream(filePath, FileMode.Create))
-            {
-                await request.File.CopyToAsync(fileStream);
-            }
-
-            var image = Path.Combine("images/blogs", uniqueFileName);
-
-            var pipeLine = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
-            request.BlogDTO.blogContent = Markdown.ToHtml(request.BlogDTO.blogContent, pipeLine);
-            var mappedBlog = _mapper.Map<BlogModel>(request.BlogDTO);
-            mappedBlog.featuredImagePath = image.Replace("\\", "/");
-            mappedBlog.slug = SlugHelper.GenerateSlug(request.BlogDTO.blogTitle);
-            mappedBlog.createAt = DateTime.Now;
-            mappedBlog.updateAt = DateTime.Now;
-            mappedBlog.publishedAt = DateTime.Now;
-            mappedBlog.blogCategories = new List<BlogCategoryJoinModel>();
-
-            foreach (var categoryId in request.BlogDTO.blogCategories)
-            {
-                var get = await _unitOfWork.blogCategory.Get(bc => bc.id == categoryId);
-                if (get != null)
+                var uploadsFolder = Path.Combine(_environment.WebRootPath, "images", "blogs");
+                if (!Directory.Exists(uploadsFolder))
                 {
-                    mappedBlog.blogCategories.Add(new BlogCategoryJoinModel
-                    {
-                        categoryId = categoryId
-                    });
+                    Directory.CreateDirectory(uploadsFolder);
                 }
-                else
-                {
-                    return new CommandsResponse<BlogDTO>
-                    {
-                        status = "Fail",
-                        message = "One Or All Of BlogCategories Not Exicted!",
-                    };
-                }
-            }
-            await _unitOfWork.blog.Add(mappedBlog);
-            await _unitOfWork.Save();
+                var uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(request.File.FileName);
+                var filePath = Path.Combine(uploadsFolder, uniqueFileName);
 
-            return new CommandsResponse<BlogDTO>
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await request.File.CopyToAsync(fileStream);
+                }
+
+                var image = Path.Combine("images/blogs", uniqueFileName);
+
+                var pipeLine = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+                request.BlogDTO.blogContent = Markdown.ToHtml(request.BlogDTO.blogContent, pipeLine);
+                var mappedBlog = _mapper.Map<BlogModel>(request.BlogDTO);
+                mappedBlog.featuredImagePath = image.Replace("\\", "/");
+                mappedBlog.slug = SlugHelper.GenerateSlug(request.BlogDTO.blogTitle);
+                mappedBlog.createAt = DateTime.Now;
+                mappedBlog.updateAt = DateTime.Now;
+                mappedBlog.publishedAt = DateTime.Now;
+                mappedBlog.blogCategories = new List<BlogCategoryJoinModel>();
+
+                foreach (var categoryId in request.BlogDTO.blogCategories)
+                {
+                    var get = await _unitOfWork.blogCategory.Get(bc => bc.id == categoryId);
+                    if (get != null)
+                    {
+                        mappedBlog.blogCategories.Add(new BlogCategoryJoinModel
+                        {
+                            categoryId = categoryId
+                        });
+                    }
+                    else
+                    {
+                        return new CommandsResponse<BlogDTO>
+                        {
+                            status = "Fail",
+                            message = "One Or All Of BlogCategories Not Exicted!",
+                        };
+                    }
+                }
+                await _unitOfWork.blog.Add(mappedBlog);
+                await _unitOfWork.Save();
+
+                return new CommandsResponse<BlogDTO>
+                {
+                    status = "Success",
+                    message = "Blog Created Successfully.",
+                    data = _mapper.Map<BlogDTO>(await _unitOfWork.blog.Get(b => b.id == mappedBlog.id, includeProp: "author, blogCategories.category"))
+                };
+            }
+            catch (Exception ex)
             {
-                status = "Success",
-                message = "Blog Created Successfully.",
-                data = _mapper.Map<BlogDTO>(await _unitOfWork.blog.Get(b => b.id == mappedBlog.id, includeProp: "author, blogCategories.category"))
-            };
+                return new CommandsResponse<BlogDTO>
+                {
+                    status = "Fail",
+                    message = ex.Message
+                };
+            }
         }
     }
 }
--- NumberLand/MappingProfile/MappingProfile.cs
+++ NumberLand/MappingProfile/MappingProfile.cs
@@ -12,43 +12,60 @@ namespace NumberLand.MappingProfile
     {
         public MappingProfile()
         {
-            CreateMap<CreateNumberDTO, NumberModel>();
             CreateMap<NumberModel, NumberDTO>()
                 .ForMember(dest => dest.numberSlug, opt => opt.MapFrom(src => src.slug));
+            CreateMap<CreateNumberDTO, NumberModel>();
+            CreateMap<UpdateNumberCommand, NumberModel>();
             CreateMap<CreateBlogDTO, BlogModel>()
-                .ForMember(dest => dest.blogCategories, opt => opt.Ignore())
                 .ForMember(dest => dest.id, opt => opt.MapFrom(src => src.blogId))
                 .ForMember(dest => dest.title, opt => opt.MapFrom(src => src.blogTitle))
                 .ForMember(dest => dest.content, opt => opt.MapFrom(src => src.blogContent))
-                .ForMember(dest => dest.authorId, opt => opt.MapFrom(src => src.blogAuthorId));
+                .ForMember(dest => dest.authorId, opt => opt.MapFrom(src => src.blogAuthorId))
+                .ForMember(dest => dest.blogCategories, opt => opt.MapFrom(src => src.blogCategories.Select(id => new BlogCategoryJoinModel
+                {
+                    categoryId = id
+                })))
+                .ForMember(dest => dest.createAt, opt => opt.MapFrom(src => src.createAt))
+                .ForMember(dest => dest.updateAt, opt => opt.MapFrom(src => src.updateAt))
+                .ForMember(dest => dest.publishedAt, opt => opt.MapFrom(src => src.publishedAt))
+                .ForMember(dest => dest.isPublished, opt => opt.MapFrom(src => src.blogIsPublished));
 
             CreateMap<BlogModel, BlogDTO>()
                 .ForMember(dest => dest.blogId, opt => opt.MapFrom(src => src.id))
+                .ForMember(dest => dest.blogSlug, opt => opt.MapFrom(src => src.slug))
                 .ForMember(dest => dest.blogTitle, opt => opt.MapFrom(src => src.title))
                 .ForMember(dest => dest.blogContent, opt => opt.MapFrom(src => src.content))
-                .ForMember(dest => dest.blogCategories, opt => opt.MapFrom(src => src.blogCategories))
                 .ForMember(dest => dest.blogAuthor, opt => opt.MapFrom(src => src.author))
-                .ForMember(dest => dest.blogCategories, opt => opt.MapFrom(src => src.blogCategories))
-                .ForMember(dest => dest.blogSlug, opt => opt.MapFrom(src => src.slug))
+                .ForMember(dest => dest.blogCategories, opt => opt.MapFrom(src => src.blogCategories.Select(bc => new BlogCategoryDTO
+                {
+                    blogCategoryId = bc.category.id,
+                    blogCategoryName = bc.category.name,
+                    blogCategorySlug = bc.category.slug,
+                    blogCategoryDescription = bc.category.description
+                })))
                 .ForMember(dest => dest.blogFeaturedImagePath, opt => opt.MapFrom(src => src.featuredImagePath))
+                .ForMember(dest => dest.createAt, opt => opt.MapFrom(src => src.createAt))
+                .ForMember(dest => dest.updateAt, opt => opt.MapFrom(src => src.updateAt))
+                .ForMember(dest => dest.publishedAt, opt => opt.MapFrom(src => src.publishedAt))
                 .ForMember(dest => dest.blogIsPublished, opt => opt.MapFrom(src => src.isPublished));
-            CreateMap<CreateBlogCategoryDTO, BlogCategoryModel>()
-                .ForMember(dest => dest.id, opt => opt.MapFrom(src => src.blogCategoryId))
-                .ForMember(dest => dest.name, opt => opt.MapFrom(src => src.blogCategoryName))
-                .ForMember(dest => dest.description, opt => opt.MapFrom(src => src.blogCategoryDescription));
             CreateMap<BlogCategoryModel, BlogCategoryDTO>()
                 .ForMember(dest => dest.blogCategoryId, opt => opt.MapFrom(src => src.id))
                 .ForMember(dest => dest.blogCategoryName, opt => opt.MapFrom(src => src.name))
                 .ForMember(dest => dest.blogCategorySlug, opt => opt.MapFrom(src => src.slug))
                 .ForMember(dest => dest.blogCategoryDescription, opt => opt.MapFrom(src => src.description));
-            ;
+            CreateMap<BlogCategoryModel, BlogCategoryDTO>()
+                .ForMember(dest => dest.blogCategoryId, opt => opt.MapFrom(src => src.id))
+                .ForMember(dest => dest.blogCategoryName, opt => opt.MapFrom(src => src.name))
+                .ForMember(dest => dest.blogCategorySlug, opt => opt.MapFrom(src => src.slug))
+                .ForMember(dest => dest.blogCategoryDescription, opt => opt.MapFrom(src => src.description));
+
             CreateMap<PageDTO, PageeModel>();
             CreateMap<PageeModel, PageDTO>();
             CreateMap<CreatePageDTO, PageeModel>();
             CreateMap<PageCategoryDTO, PageCategoryModel>();
             CreateMap<PageCategoryModel, PageCategoryDTO>();
             CreateMap<CreatePageCategoryDTO, PageCategoryModel>();
-            CreateMap<UpdateNumberCommand, NumberModel>();
+
             CreateMap<CreateAuthorDTO, AuthorModel>()
                 .ForMember(dest => dest.id, opt => opt.MapFrom(src => src.authorId))
                 .ForMember(dest => dest.name, opt => opt.MapFrom(src => src.authorName))
